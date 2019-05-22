a6cb59c4b0dbb5fb3e9f4c898f127c710839afc3
Maulik Soni <ms_soni@hotmail.com>
2019-05-22 14:20:09 -0500

    Updated to query key

--- fabrikam-functions/src/main/java/com/fabrikam/functions/Function.java
+++ fabrikam-functions/src/main/java/com/fabrikam/functions/Function.java
@@ -6,6 +6,7 @@ import com.microsoft.azure.keyvault.KeyVaultClient;
 import com.microsoft.azure.keyvault.models.KeyBundle;
 import com.microsoft.azure.keyvault.models.SecretBundle;
 import com.microsoft.azure.keyvault.webkey.JsonWebKey;
+import com.microsoft.azure.keyvault.webkey.JsonWebKeyType;
 import com.microsoft.azure.AzureEnvironment;
 import com.microsoft.azure.credentials.AppServiceMSICredentials;
 import com.microsoft.azure.functions.*;
@@ -15,10 +16,10 @@ import com.microsoft.azure.functions.*;
  */
 public class Function {
     /**
-     * This function listens at endpoint "/api/HttpTrigger-Java". Two ways to invoke
+     * This function listens at endpoint "/api/secret". Two ways to invoke
      * it using "curl" command in bash: 1. curl -d "HTTP Body" {your
-     * host}/api/HttpTrigger-Java&code={your function key} 2. curl "{your
-     * host}/api/HttpTrigger-Java?name=HTTP%20Query&code={your function key}"
+     * host}/api/secret&code={your function key} 2. curl "{your
+     * host}/api/secret?name=HTTP%20Query&code={your function key}"
      * Function Key is not needed when running locally, it is used to invoke
      * function deployed to Azure. More details:
      * https://aka.ms/functions_authorization_keys
@@ -27,7 +28,10 @@ public class Function {
     public HttpResponseMessage run(@HttpTrigger(name = "req", methods = { HttpMethod.GET,
             HttpMethod.POST }, authLevel = AuthorizationLevel.FUNCTION) HttpRequestMessage<Optional<String>> request,
             final ExecutionContext context) {
-        context.getLogger().info("Java HTTP trigger processed a request.");
+        context.getLogger().info("Java HTTP trigger processed a request."); 
+
+        context.getLogger().info(System.getenv("MSI_ENDPOINT"));
+        context.getLogger().info(System.getenv("MSI_SECRET"));
 
         // Parse query parameter
         String query = request.getQueryParameters().get("version");
@@ -35,19 +39,30 @@ public class Function {
 
         AppServiceMSICredentials credentials = new AppServiceMSICredentials(AzureEnvironment.AZURE);
         KeyVaultClient keyVaultClient = new KeyVaultClient(credentials);
+
+        
+        // Create Key Version
+        // for(int x = 1; x < 65000; x = x + 1) {
+        //     System.out.print( x );
+        //     KeyBundle kb = keyVaultClient.createKey("https://javafndemokv.vault.azure.net/", "enckey",JsonWebKeyType.RSA);
+        //     context.getLogger().info(kb.key().toString());
+        //  }
+        
         
 
         if (version == null) {
-            SecretBundle result = keyVaultClient.getSecret("https://javafndemokv.vault.azure.net/", "enckey");
-            context.getLogger().info("keyvault secret value");
-            context.getLogger().info(result.value());
-            return request.createResponseBuilder(HttpStatus.OK).body("Secret :" + result.value()).build();
+            KeyBundle result = keyVaultClient.getKey("https://javafndemokv.vault.azure.net/", "enckey");
+            String k = result.key().toString();
+            context.getLogger().info("keyvault Key value");
+            context.getLogger().info(k);
+            return request.createResponseBuilder(HttpStatus.OK).body("Key :" + k).build();
 
         } else {
-            SecretBundle result = keyVaultClient.getSecret("https://javafndemokv.vault.azure.net/", "enckey",version);
-            context.getLogger().info("keyvault secret value");
-            context.getLogger().info(result.value());
-            return request.createResponseBuilder(HttpStatus.OK).body("Version :  " + version + " Secret :" + result.value()).build();
+            KeyBundle result = keyVaultClient.getKey("https://javafndemokv.vault.azure.net/", "enckey",version);
+            String k = result.key().toString();
+            context.getLogger().info("keyvault Key value");
+            context.getLogger().info(k);
+            return request.createResponseBuilder(HttpStatus.OK).body("Version :  " + version + " Key :" + k).build();
         }
     }
 }
